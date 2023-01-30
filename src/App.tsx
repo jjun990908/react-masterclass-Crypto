e5fa9b1811b2db193b24f6f60a1fb65e5b696481
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2023-01-30 19:14:16 +0900

    use props

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,11 @@
-import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.theme.backgroundColor};
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    border-radius:100px;
-  } 
-  100%{
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 100px;
-  }
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-      <Box>
-        <Emoji>😁</Emoji>
-      </Box>
-      <Emoji>😅</Emoji>
-    </Wrapper>
+    <div>
+      <Circle borderColor="yellow" bgColor="teal"/>
+      <Circle text ="I'm here" bgColor="tomato"/>
+    </div>
   );
 }
 
